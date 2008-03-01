33ba9994bb9f67933d480ecd3874d994f757d35e
mattf <mattf@4e257579-0214-4147-9015-27880e0d027f>
2008-03-01 19:46:08 +0000

    Initial checkin of code to pass Q.921 PC25 and PC26

--- pri.c
+++ pri.c
@@ -187,6 +187,15 @@ static int __pri_write(struct pri *pri, void *buf, int buflen)
 	return res;
 }
 
+/* Pass in the master for this function */
+void __pri_free_tei(struct pri * p, int tei)
+{
+	if (p->subchannel && (tei == p->subchannel->tei)) {
+		free(p->subchannel);
+		p->subchannel = NULL;
+	}
+}
+
 struct pri *__pri_new_tei(int fd, int node, int switchtype, struct pri *master, pri_io_cb rd, pri_io_cb wr, void *userdata, int tei, int bri)
 {
 	struct pri *p;
