011ca1c974e00b98a23b6b3734e3d01acd5a65e9
root <root@4e257579-0214-4147-9015-27880e0d027f>
2009-06-25 19:35:44 +0000

    Merged revisions 907 via svnmerge from file:///srv/subversion/repos/libpri/branches/1.4
    ........
      r907 | seanbright | 2009-06-25 13:53:38 -0500 (Thu, 25 Jun 2009) | 17 lines
      
      Add support for sending Reverse Charging Indication IE on ISDN PRI.
      
      Add the ability to transmit a Reverse Charging Indication IE during a SETUP
      message.  In passing, re-work some of the receive logic to be forwards
      compatible with new RCI values that may be added in the future.  Also removed
      the PRI_REVERSECHARGE_SUPPORT define that I added on the last commit since we
      can just check for PRI_REVERSECHARGE_NONE or _REQUESTED on the Asterisk side to
      determine support for this.
      
      Special thanks to rmudgett who could have written this in half the time he spent
      reviewing it, but instead talked me through it.  Much appreciated!
      
      (issue #13760)
      Reported by: mrgabu
      
      Review: https://reviewboard.asterisk.org/r/292/
    ........

--- team/group/issue14068/pri_internal.h
+++ team/group/issue14068/pri_internal.h
@@ -158,6 +158,7 @@ struct pri_sr {
 	int justsignalling;
 	const char *useruserinfo;
 	int transferable;
+	int reversecharge;
 };
 
 /* Internal switch types */
@@ -423,7 +424,10 @@ struct q931_call {
 	q931_call *bridged_call;        /* Pointer to other leg of bridged call (Used by Q.SIG when eliminating tromboned calls) */
 
 	int changestatus;		/* SERVICE message changestatus */
-	int reversecharge;		/* Reverse charging indication */
+	int reversecharge;		/* Reverse charging indication:
+							   -1 - No reverse charging
+							    1 - Reverse charging
+							0,2-7 - Reserved for future use */
 };
 
 extern int pri_schedule_event(struct pri *pri, int ms, void (*function)(void *data), void *data);
