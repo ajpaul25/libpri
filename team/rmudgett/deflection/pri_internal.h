1ab03edee8ac18befb94db157da958e228dc8b99
root <root@4e257579-0214-4147-9015-27880e0d027f>
2009-10-07 20:30:20 +0000

    Merged revisions 1160 via svnmerge from file:///srv/subversion/repos/libpri/team/rmudgett/facility
    ........
      r1160 | rmudgett | 2009-10-07 14:55:44 -0500 (Wed, 07 Oct 2009) | 1 line
      
      Add call to explicitly enable HOLD/RETRIEVE support.
    ........

--- team/rmudgett/deflection/pri_internal.h
+++ team/rmudgett/deflection/pri_internal.h
@@ -70,6 +70,7 @@ struct pri {
 	int protodisc;
 	unsigned int bri:1;
 	unsigned int acceptinbanddisconnect:1;	/* Should we allow inband progress after DISCONNECT? */
+	unsigned int hold_support:1;/* TRUE if upper layer supports call hold. */
 	
 	/* Q.921 State */
 	int q921_state;	
